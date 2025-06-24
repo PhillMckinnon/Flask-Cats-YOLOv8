e95ca0b019d57b26281ec55896ac4f4810b744f5
PhillMckinnon <t2130101@gmail.com>
2025-06-24 14:25:17 +0300

    get_model functionality

--- main_app/app.py
+++ main_app/app.py
@@ -7,7 +7,14 @@ import os
 import io
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'model_folder/uploaded_images_folder'
+model = None
 
+def get_model():
+    global model
+    if model is None:
+        model_path = os.path.join(os.path.dirname(__file__), "model_folder", "best.pt")
+        model = YOLO(model_path)
+    return model
 def detect_and_annotate(image, conf=0.25, rectangle_thickness=3, text_size=70):
     image_pil = image if (isinstance(image, Image.Image)) else Image.fromarray(image)
     cat_detected = False
@@ -17,9 +24,7 @@ def detect_and_annotate(image, conf=0.25, rectangle_thickness=3, text_size=70):
         font = ImageFont.truetype(font_path, text_size)
     except OSError:
         font = ImageFont.load_default()
-    model_path = os.path.join(os.path.dirname(__file__), "model_folder", "best.pt")
-    model = YOLO(model_path)
-    results = model.predict(image, conf=conf)
+    results = get_model().predict(image, conf=conf)
     for result in results:
         for box in result.boxes:
             draw.rectangle(
